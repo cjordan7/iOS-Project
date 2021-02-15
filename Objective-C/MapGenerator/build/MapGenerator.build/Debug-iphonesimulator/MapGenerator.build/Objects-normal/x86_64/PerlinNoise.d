b82cb11848bf61d6f0a80e9f10ae6f79af379c60
cjordan7 <cosmehbvjordan@gmail.com>
2021-02-15 14:33:34 +0100

    Delete build directory

--- Objective-C/MapGenerator/build/MapGenerator.build/Debug-iphonesimulator/MapGenerator.build/Objects-normal/x86_64/PerlinNoise.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/cosmejordan/Documents/Programming/iOS-Project-Github/Objective-C/MapGenerator/MapGenerator/Model/PerlinNoise.m \
-  /Users/cosmejordan/Documents/Programming/iOS-Project-Github/Objective-C/MapGenerator/MapGenerator/Model/PerlinNoise.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.3.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.3.sdk/usr/include/mach-o/dyld.modulemap
--- Objective-C/MapGenerator/build/MapGenerator.build/Debug-iphonesimulator/MapGenerator.build/Objects-normal/x86_64/ViewController.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/cosmejordan/Documents/Programming/iOS-Project-Github/Objective-C/MapGenerator/MapGenerator/ViewController.m \
-  /Users/cosmejordan/Documents/Programming/iOS-Project-Github/Objective-C/MapGenerator/MapGenerator/ViewController.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.3.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.3.sdk/usr/include/mach-o/dyld.modulemap
